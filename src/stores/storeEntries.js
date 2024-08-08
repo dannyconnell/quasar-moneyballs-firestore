4ad0858c77e3ed8c377a51a282abfca91efd1354
Danny Connell <sendstufftodanny@gmail.com>
2024-08-08 20:12:00 +0100

    3.8

--- src/stores/storeEntries.js
+++ src/stores/storeEntries.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref, computed, reactive, nextTick } from 'vue'
 import { uid, Notify } from 'quasar'
-import { collection, onSnapshot, addDoc, doc, deleteDoc } from 'firebase/firestore'
+import { collection, onSnapshot, addDoc, doc, deleteDoc, updateDoc } from 'firebase/firestore'
 import { db } from 'src/firebase/firebase'
 
 const entriesCollectionRef = collection(db, 'entries')
@@ -111,9 +111,8 @@ export const useStoreEntries = defineStore('entries', () => {
       })
     }
 
-    const updateEntry = (entryId, updates) => {
-      const index = getEntryIndexById(entryId)
-      Object.assign(entries.value[index], updates)
+    const updateEntry = async (entryId, updates) => {
+      await updateDoc(doc(entriesCollectionRef, entryId), updates)
     }
 
     const sortEnd = ({ oldIndex, newIndex }) => {
