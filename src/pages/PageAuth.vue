12c1b38064b5cd68c0b8cdb13b0cab171cf814a3
Danny Connell <sendstufftodanny@gmail.com>
2024-08-09 10:27:54 +0100

    5.3

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -3,7 +3,20 @@
     <q-card
       class="auth bg-primary text-white q-pa-lg"
     >
-      <ToolbarTitle />
+      <q-card-section>
+        <ToolbarTitle />
+      </q-card-section>
+
+      <q-card-section>
+        <q-tabs
+          v-model="tab"
+          no-caps
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
+      </q-card-section>
+
     </q-card>
   </q-page>
 </template>
@@ -14,6 +27,14 @@
     imports
   */
   
+    import { ref } from 'vue'
     import ToolbarTitle from 'src/components/Layout/ToolbarTitle.vue'
+
+  
+  /*
+    tabs
+  */
+  
+    const tab = ref('login')
 
 </script>
