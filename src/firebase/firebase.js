e32a12cb7d087d4b9dc47a2b035a7b76b0603f3d
Danny Connell <sendstufftodanny@gmail.com>
2024-08-11 16:08:28 +0100

    10.2

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getAuth, initializeAuth, indexedDBLocalPersistence } from 'firebase/auth'
+import { Capacitor } from '@capacitor/core'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDfH3zGn8105ao6uXAy-hQzcXSDmg4ooVc',
@@ -11,9 +12,21 @@ const firebaseConfig = {
   appId: '1:480300453984:web:b7553ca734fafe704b7305'
 }
 
+function whichAuth() {
+  let auth
+  if (Capacitor.isNativePlatform()) {
+    auth = initializeAuth(app, {
+      persistence: indexedDBLocalPersistence
+    })
+  } else {
+    auth = getAuth()
+  }
+  return auth
+}
+
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
-const auth = getAuth(app)
+const auth = whichAuth()
 
 export {
   db,
