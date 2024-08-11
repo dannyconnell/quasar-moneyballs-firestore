b93dfafc6c2b397a9b1370764b384e11afa67032
Danny Connell <sendstufftodanny@gmail.com>
2024-08-11 16:46:18 +0100

    10.3

--- src/stores/storeEntries.js
+++ src/stores/storeEntries.js
@@ -98,8 +98,8 @@ export const useStoreEntries = defineStore('entries', () => {
       loadEntries()
     }
 
-    const loadEntries = async () => {
-      entriesLoaded.value = false
+    const loadEntries = async (showLoader = true) => {
+      if (showLoader) entriesLoaded.value = false
       getEntriesSnapshot = onSnapshot(entriesCollectionRef, (querySnapshot) => {
         let entriesFB = []
         querySnapshot.forEach((doc) => {
@@ -146,16 +146,18 @@ export const useStoreEntries = defineStore('entries', () => {
       await updateDoc(doc(entriesCollectionRef, entryId), updates)
     }
 
-    const updateEntryOrderNumbers = () => {
+    const updateEntryOrderNumbers = async () => {
       let currentOrder = 1
       entries.value.forEach(entry => {
         entry.order = currentOrder
         currentOrder++
       })
 
-      entries.value.forEach(entry => {
-        updateEntry(entry.id, { order: entry.order })
-      })
+      getEntriesSnapshot()
+      await Promise.all(entries.value.map(async entry => {
+        await updateEntry(entry.id, { order: entry.order })
+      }))
+      loadEntries(false)
     }
 
     const sortEnd = ({ oldIndex, newIndex }) => {
